34582ee3c6b7cedecdceca1bc36b26d1218fdd27
Myles Hayes <57469711+mshayes18@users.noreply.github.com>
2019-11-10 13:23:01 -0500

    Add files via upload

--- twint_script.py
+++ twint_script.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     ''' Script to run scrape function. Picks up where killed last '''
     # Enter parameters for scraping
     keyword = 'bitcoin'
-    start = datetime.datetime(year = 2019, month = 1, day = 24)
+    start = datetime.datetime(year = 2019, month = 2, day = 16)
     end = datetime.datetime(year = 2019, month = 4, day = 1)
-    # Don't modify
+    # Don't modify (default to 0 if not left off mid-script)
     off_by = 0
 
     # Ensure file exists, create if not
@@ -19,21 +19,17 @@ if __name__ == '__main__':
     left_off = file.read()
     file.close()
     if left_off != '':
-        # Set start date to time in which it terminated at
+        # Set start date to time in which it terminated at, but as new
+        # parameter so that same file is modified in twint_scrape
         s_year = int(left_off[:4])
         s_month = int(left_off[5:7])
         s_day = int(left_off[8:10])
         s_hour = int(left_off[11:13])
         s_minute = int(left_off[14:16])
-        start = datetime.datetime(year = s_year, month = s_month, day = s_day, hour = s_hour, minute = s_minute)
-
-        # Write this time & value in a file to show script was killed,
-        # use the value as input to scrape since that is tweets scraped
-        # thus far on that date
-        file = open(str(keyword) + '_off_by.txt', 'a')
-        file.write(left_off + '\n')
-        file.close()
+        new_start = datetime.datetime(year = s_year, month = s_month, day = s_day, hour = s_hour, minute = s_minute)
+        # Tweets scraped thus far on this date
         off_by = int(left_off[20:])
+        output = scrape(new_start, end, keyword, start, off_by)
         
-    output = scrape(start, end, keyword, off_by)
-    print(output)
+    else:
+        output = scrape(new_start, end, keyword)
